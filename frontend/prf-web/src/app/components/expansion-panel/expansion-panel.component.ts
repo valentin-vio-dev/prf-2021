1748cf2629f558059defa6113d6da3b7bb0c79f9
Valentin Hegedus <valentinhegedus29@gmail.com>
2021-04-26 15:38:44 +0200

    qwe

--- frontend/prf-web/src/app/components/expansion-panel/expansion-panel.component.ts
+++ frontend/prf-web/src/app/components/expansion-panel/expansion-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-expansion-panel',
@@ -6,15 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expansion-panel.component.scss']
 })
 export class ExpansionPanelComponent implements OnInit {
+  @ViewChild('container') container: any;
   open: boolean = false;
+  height: number | any = 0;
+  visibility: string = 'invisible';
 
-  constructor() { }
+  constructor(private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.height = this.container.nativeElement.clientHeight;
+    setTimeout(() => {
+      this.visibility = 'visible';
+    }, 0);
+    this.cdr.detectChanges();
+  }
+
   toggle() {
     this.open = !this.open;
+  }
+
+  getHeight() {
+    return -this.height + 'px';
   }
 
 }
