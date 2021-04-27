2889e009b908640528e1dcc7f569f47f6bcb3fb8
Valentin Hegedus <valentinhegedus29@gmail.com>
2021-04-27 22:32:01 +0200

    qwe

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ require('./models/user.model');
 require('./models/product.model');
 require('./models/order.model');
 
-app.use(cookieParser());
+app.use(cookieParser({ secret: process.env.APP_SECRET}));
 app.use(express.urlencoded({ extended: true, limit: '50mb'}));
 app.use(express.json({ limit: '50mb' }));
 app.use(expressSession({ secret: process.env.APP_SECRET, resave: true, saveUninitialized: false }));
