d1ea9d7538e87fe9f9a6f1f0595693a579812686
Valentin Hegedus <valentinhegedus29@gmail.com>
2021-04-27 23:40:13 +0200

    qwe

--- frontend/prf-web/server.js
+++ frontend/prf-web/server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/prf-web'));
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/prf-web/index.html'));
 });
-app.listen(process.env || 8080);
+app.listen(process.env.PORT || 8080);
